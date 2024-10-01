3b76ed8768db43b9f72390f81ba2f8e331e74089
Lytvynenko Serhiy <serrheylitvinenko@gmail.com>
2024-10-01 04:24:47 +0300

    lab1: third task

---
+++ 1-lab/3-callback-signal.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const once = (fn) => {
+  let called = false;
+  return (...args) => {
+    if (called) return null;
+    called = true;
+    return fn(...args);
+  };
+};
+
+const enumerate = (array) => ({
+  [Symbol.iterator]: () => ({
+    index: 0,
+    next() {
+      return {
+        done: this.index >= array.length,
+        value: [this.index, array[this.index++]],
+      };
+    },
+  }),
+});
+
+const map = (array, process, signal, onFinish) => {
+  const items = [];
+  let finished = false;
+  signal.addEventListener('abort', () => {
+    finished = true;
+    onFinish(signal.reason, null);
+  });
+  for (const [index, item] of enumerate(array)) {
+    process(item, once((error, processed) => {
+      if (finished) return;
+      if (error) {
+        finished = true;
+        return void onFinish(error, null);
+      }
+      items.push({ index, item: processed });
+      if (items.length === array.length) {
+        const sorted = items.sort((a, b) => a.index - b.index);
+        const result = sorted.map((value) => value.item);
+        onFinish(null, result);
+      }}),
+    );
+  }
+};
+
+const processArray = (item, callback) => {
+  setTimeout(callback, 1000 - 100 * item, null, item * 2);
+};
+
+map([1, 2, 3, 4], processArray, AbortSignal.timeout(500), (error, result) => {
+  console.log({ error, result });
+});
